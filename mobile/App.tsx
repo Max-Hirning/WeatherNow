7cb92bd7414b1d7ebae5bc814fca7a0d5dae4dce
Max Hirning <maxhirning25@gmail.com>
2023-07-12 15:03:53 +0300

    Added splash screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,10 +2,15 @@ import { Provider } from "react-redux";
 import { StatusBar } from 'react-native';
 import { store } from "./src/redux/store";
 import Navigation from "./src/Navigation";
-import React, { ReactElement } from 'react';
+import SplashScreen from 'react-native-splash-screen';
+import React, { ReactElement, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function App(): ReactElement {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <Provider store={store}>
