702cdc948525419e4bdd89690f0fdf1811e91bde
Max Hirning <maxhirning25@gmail.com>
2023-07-18 16:59:07 +0300

    Added tailwind css

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-slate-950'>
+      <p className='text-red-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id mollitia vitae vel ab voluptatibus illo ipsam amet accusamus ducimus exercitationem temporibus nam est molestiae soluta asperiores, minima quasi eius sapiente!</p>
     </div>
   );
 }
