fe702cc587b85556ab5503d0e684694ed0a78d00
Max Hirning <maxhirning25@gmail.com>
2023-07-18 17:04:22 +0300

    Added eslint

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-  return (
-    <div className='bg-slate-950'>
-      <p className='text-red-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id mollitia vitae vel ab voluptatibus illo ipsam amet accusamus ducimus exercitationem temporibus nam est molestiae soluta asperiores, minima quasi eius sapiente!</p>
-    </div>
-  );
+	return (
+		<div className='bg-slate-950'>
+			<p className='text-red-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id mollitia vitae vel ab voluptatibus illo ipsam amet accusamus ducimus exercitationem temporibus nam est molestiae soluta asperiores, minima quasi eius sapiente!</p>
+		</div>
+	);
 }
 
 export default App;
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,22 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
-    "./src/*.{js,jsx,ts,tsx}",
-    "./src/UI/*.{js,jsx,ts,tsx}",
-    "./src/pages/*.{js,jsx,ts,tsx}",
-    "./src/components/*.{js,jsx,ts,tsx}",
-    "./src/modules/Loader/view/*.{js,jsx,ts,tsx}",
-    "./src/modules/StartInfo/view/*.{js,jsx,ts,tsx}",
-    "./src/modules/SideBarMenu/view/*.{js,jsx,ts,tsx}",
-    "./src/modules/WeekForecast/view/*.{js,jsx,ts,tsx}",
-    "./src/modules/TodayForecast/view/*.{js,jsx,ts,tsx}",
-    "./src/modules/TodayForecastSlider/view/*.{js,jsx,ts,tsx}",
-    "./src/modules/TodayForecastDetails/view/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: [
+		"./src/*.{js,jsx,ts,tsx}",
+		"./src/UI/*.{js,jsx,ts,tsx}",
+		"./src/pages/*.{js,jsx,ts,tsx}",
+		"./src/components/*.{js,jsx,ts,tsx}",
+		"./src/modules/Loader/view/*.{js,jsx,ts,tsx}",
+		"./src/modules/StartInfo/view/*.{js,jsx,ts,tsx}",
+		"./src/modules/SideBarMenu/view/*.{js,jsx,ts,tsx}",
+		"./src/modules/WeekForecast/view/*.{js,jsx,ts,tsx}",
+		"./src/modules/TodayForecast/view/*.{js,jsx,ts,tsx}",
+		"./src/modules/TodayForecastSlider/view/*.{js,jsx,ts,tsx}",
+		"./src/modules/TodayForecastDetails/view/*.{js,jsx,ts,tsx}",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
